3d8d74a1cdefa528a6083cfd00c18644a92dbe29
Matt Robinson <mthw.wm.robinson@gmail.com>
2021-06-26 14:13:09 -0400

    linting, linting, linting ...

--- scraper/main.go
+++ scraper/main.go
@@ -2,24 +2,22 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
-  "io/ioutil"
 )
 
-
 func fetchUrl(url string) string {
-  // Makes an http call to the specified url and returns a string representation of the
-  // HTML response.
-  resp, _ := http.Get(url)
-  bytes, _ := ioutil.ReadAll(resp.Body)
-  return string(bytes)
+	// Makes an http call to the specified url and returns a string representation of the
+	// HTML response.
+	resp, _ := http.Get(url)
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	return string(bytes)
 }
 
-
 func main() {
-  var url, html string
-  url = os.Args[1]
-  html = fetchUrl(url)
-  fmt.Println(html)
+	var url, html string
+	url = os.Args[1]
+	html = fetchUrl(url)
+	fmt.Println(html)
 }
