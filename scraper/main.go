206659ce86c6eb6e0fc92a828b5a540b4dbfbedf
Matt Robinson <mthw.wm.robinson@gmail.com>
2021-06-29 23:09:00 -0400

    more fun with http responses

--- scraper/main.go
+++ scraper/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"golang.org/x/net/html"
+	"io/ioutil"
 	"net/http"
-	"os"
+	//"encoding/json"
+	// "os"
 )
+
+const baseURL string = "https://www.federalregister.gov/documents/search?conditions[publication_date[is]=%s&page=%s#"
 
 type PageFetcher func(url string) (*http.Response, error)
 
@@ -18,6 +22,30 @@ func FetchUrl(url string, pageFetcher PageFetcher) (*http.Response, error) {
 	}
 
 	return resp, nil
+}
+
+func getRegulations(date string, page int) []string {
+	// Collects a list of of document and links from the Federal Register for the
+	// specified date and page number
+	var urlList []string
+	url := "https://www.federalregister.gov/api/v1/documents?conditions%5Bpublication_date%5D%5Bis%5D=2021-06-29&format=json&page=2"
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(bytes))
+
+	return urlList
+
 }
 
 func FindLinks(response *http.Response) {
@@ -41,7 +69,8 @@ func FindLinks(response *http.Response) {
 }
 
 func main() {
-	url := os.Args[1]
-	response, _ := FetchUrl(url, http.Get)
-	FindLinks(response)
+	// url := os.Args[1]
+	getLinks("2021-06-29", 1)
+	// response, _ := FetchUrl(url, http.Get)
+	// FindLinks(response)
 }
