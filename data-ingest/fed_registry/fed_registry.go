feebe91bff19196821b707123042917656b44d4c
Matt Robinson <mthw.wm.robinson@gmail.com>
2021-09-04 16:33:55 -0400

    moved registry code to separate package

--- data-ingest/fed_registry/fed_registry.go
+++ data-ingest/fed_registry/fed_registry.go
@@ -1,4 +1,4 @@
-package main
+package fed_registry
 
 import (
 	"encoding/json"
@@ -75,7 +75,7 @@ func main() {
 	client := &http.Client{}
 	r := registerFetcher{client: client}
 	results := r.getDailyRegisterResults("2021-06-02")
-  directory.WriteJSON(results, "/home/matt/tmp/test.json")
+	directory.WriteJSON(results, "/home/matt/tmp/test.json")
 
 	// fmt.Println(registerResults.NextPageURL)
 	// files, _ := ioutil.ReadDir("../../../")
--- data-ingest/fed_registry/fed_registry_test.go
+++ data-ingest/fed_registry/fed_registry_test.go
@@ -1,4 +1,4 @@
-package main
+package fed_registry
 
 import (
 	"bytes"
